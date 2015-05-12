40940a767a8ad1e7f34d7adb3fcbbedf6520f127
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-05-12 20:26:12 +0200

    Added javadoc and work on the GUI

---
+++ WWeatherStation/WeatherStation/src/ch/coursjava/meteo/graph/customs/QKTCheckBox.java
@@ -0,0 +1,21 @@
+
+package ch.coursjava.meteo.graph.customs;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+
+public class QKTCheckBox extends JCheckBox
+	{
+
+	public QKTCheckBox(String text)
+		{
+		super(text);
+
+		this.setForeground(Color.DARK_GRAY);
+		this.setIcon(new ImageIcon("assets/unchecked_checkbox.png"));
+		this.setSelectedIcon(new ImageIcon("assets/checked_checkbox.png"));
+		}
+
+	}
